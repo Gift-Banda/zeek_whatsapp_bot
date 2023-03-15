859554dd153ebc5b986ab361e27014821bea128d
Gift Banda <bandaolga19@.com>
2023-03-15 16:05:00 +0200

    added new auth method

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const Info_Long = require("./features/Wiki_Long");
 venom
   .create({
     session: "Production" //name of session
+  },
+  (base64Qrimg, asciiQR, attempts, urlCode) => {
+    console.log('Number of attempts to read the qrcode: ', attempts);
+    console.log('Terminal qrcode: ', asciiQR);
+    console.log('base64 image string qrcode: ', base64Qrimg);
+    console.log('urlCode (data-ref): ', urlCode);
   })
   .then(client => start(client))
   .catch(erro => {
